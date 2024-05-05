f7288f4a68a733f190ec89949f6a545029718ade
nitrolanco <mamibalss@gmail.com>
2024-05-05 19:04:22 -0400

    Test 4 approved
    Se hizo un pequeño cambio a greet() dado que se detectó que no tenía en cuenta el caso en el que la hora fuera exactamente las 24. En un uso real, se utilizaría la hora actual obtenida con la librería datetime, por lo que es un caso extremanadamente raro

--- functions.py
+++ functions.py
@@ -2,7 +2,7 @@ import datetime
 
 def greet(time, name):
     current_hour = time
-    if 20 <= current_hour < 24 or 0 <= current_hour < 6:
+    if 20 <= current_hour <= 24 or 0 <= current_hour < 6:
         return f"¡Buenas noches {name}!"
     elif 6 <= current_hour < 12:
         return f"¡Buenos dias {name}!"
@@ -22,4 +22,16 @@ def handle_stop(name):
     return f'Adios {name}'
 
 def main_loop(time, name, word_1, word_2, word_3, word_4):
-    return None
+    log = greet(time, name)
+    words = [word_1, word_2, word_3, word_4]
+    i = 0
+    while True:
+        word = words[i]
+        if word == "stop!":
+            log = log + f"\n{handle_stop(name)}"
+            break
+        else:
+            log = log + f"\n{handle_word(word)}"
+        i += 1
+    return log
+
